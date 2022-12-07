df3e90fb3f83777a7a231c5fe2888d9c744352fb
levy kipkoech <kipkoechlevy096@gmail.com>
2022-12-07 08:57:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+//Write your code here
+  const num1 = 31
+  const num2 =2
+  const multiply = num1*num2
+
+  const random = Math.ceil(Number.isInteger(2))
+
+  const num3=14
+  const num4=5
+  const mod = num3%num4
+
+  const max = Math.max(3,4,10,20)
+ 
